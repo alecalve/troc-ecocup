bb537c64ec73d055e746e6afe7a1fa7998d0db52
Sylveurdance <sylvain.tiset@gmail.com>
2014-06-07 21:47:12 +0200

    ajout du model

--- models.py
+++ models.py
@@ -8,9 +8,44 @@ class User(db.Model):
     login = db.Column(db.String(8), primary_key=True)
     date_join = db.Column(db.DateTime(), default=datetime.datetime.utcnow())
     last_login = db.Column(db.DateTime(),default=datetime.datetime.utcnow())
-
-    def __init__(self, login):
-        self.login = login
+    
+    collections = db.relationship('Collection', backref='user', lazy='dynamic')
         
     def __repr__(self):
         return '<User %r>' % self.login
+
+class Ecocup(db.Model):
+    nom = db.Column(db.String(50), primary_key=True)
+    semestre = db.Column(db.String(3))
+    asso = db.Column(db.String(50))
+    nb_exemplaire = db.Column(db.Integer())
+    appreciation = db.Column(db.Integer())
+    
+    collections = db.relationship('Collection', backref='nom_ecocup', lazy='dynamic')
+        
+    def __repr__(self):
+        return '<Ecocup %r>' % self.nom
+        
+class Collection(db.Model):
+	id = db.Column(db.Integer(), primary_key=True)
+    user = db.Column(db.String(8), index=True, db.ForeignKey('user.login'))
+    nom_ecocup = db.Column(db.String(8), index=True, db.ForeignKey('ecocup.nom'))
+    in_collection = db.Column(db.Integer())
+    accepte_echange = db.Column(db.Integer())
+    souhaite = db.Column(db.Integer())
+	date_mise_a_jour = db.Column(db.DateTime(),default=datetime.datetime.utcnow())
+        
+    def __repr__(self):
+        return '<Collection %r>' % self.id
+        
+class Echange(db.Model):
+	id = db.Column(db.Integer(), primary_key=True)
+    user1 = db.Column(db.String(8), db.ForeignKey('user.login'))
+    user2 = db.Column(db.String(8), db.ForeignKey('user.login'))
+    nom_ecocup1 = db.Column(db.String(8), db.ForeignKey('ecocup.nom'))
+    nom_ecocup2 = db.Column(db.String(8), db.ForeignKey('ecocup.nom'))
+	date = db.Column(db.DateTime(),default=datetime.datetime.utcnow())
+        
+    def __repr__(self):
+        return '<Echange %r>' % self.id
+
