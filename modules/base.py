5d0161a232dd1ec779e6b869d1c7adb19e2a3a70
alecalve <antoine@alc.io>
2014-06-07 23:57:50 +0200

    Affichage derniers échanges

--- modules/base.py
+++ modules/base.py
@@ -4,6 +4,8 @@
 from flask import render_template, url_for, redirect, request, session, flash, Blueprint
 import conf
 from helpers import user_required
+from database import db
+from models import Ecocup, Echange
 
 bp = Blueprint('base', __name__, url_prefix='/')
 
@@ -12,5 +14,7 @@ bp = Blueprint('base', __name__, url_prefix='/')
 @user_required
 def index():
 	username = session["username"]
+	last_echanges = Echange.query.order_by(Echange.date).limit(10)
+	
 	return render_template("base/index.html", **locals())
 
